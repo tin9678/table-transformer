2a7f4bec9640ae4c8f292ca386f22b061c8d5ec9
tin9678 <43930892+tin9678@users.noreply.github.com>
2025-09-16 12:19:23 +0800

    Update and rename requirements1.txt to requirements1.sh

---
+++ requirements1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install PaddlePaddle
+python -m pip install paddlepaddle==3.0.0rc1 -i https://www.paddlepaddle.org.cn/packages/stable/cpu/
+
+# Install PaddleOCR, Ultralytics, and pandas
+pip install paddleocr ultralytics pandas
+
+# Install Streamlit
+pip install streamlit
+
+# Run the Streamlit application
+streamlit run src/streamlit_app.py
